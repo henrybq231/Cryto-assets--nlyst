5d27ee8804c7bba2960f3741954f2824a38089a6
Henry H <henrybq231@gmail.com>
2019-06-03 22:30:22 +0700

    update

--- Crypto-analyst.py
+++ Crypto-analyst.py
@@ -2,8 +2,16 @@ import pandas as pd
 
 #Export json to csv and read csv
 df = pd.read_json('https://api.coinmarketcap.com/v1/ticker')
-df.to_csv('../Desktop/crypto.csv')
-data_file = pd.read_csv('../Desktop/crypto.csv')
+
+#at company
+# internal_url = '../Desktop/crypto.csv'
+# df.to_csv(internal_url)
+#at home
+internal_url = '/Users/quyvn/Desktop/crypto.csv'
+df.to_csv(internal_url)
+
+#read csv
+data_file = pd.read_csv(internal_url)
 
 
 #Collect columns: id and market_cap_usd
@@ -17,9 +25,9 @@ top_cap_ylabel = '% of total cap'
 cap10 = cap[:10].set_index('id')
 cap10 = cap10.assign(market_cap_perc = lambda x:(x.market_cap_usd/cap.market_cap_usd.sum())*100)
 
-ax = cap10.market_cap_perc.head(10).plot.bar(title=TOP_CAP_TITLE)
+ax = cap10.market_cap_perc.head(10).plot.bar(title=top_cap_title)
 
-ax.set_ylabel(TOP_CAP_YLABEL)
+ax.set_ylabel(top_cap_ylabel)
 
 
 print(cap10)
